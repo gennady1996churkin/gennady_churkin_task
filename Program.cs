0c1e91260421771d5c30bb23066d8222b325a580
Геннадий Чуркин <gennadijcurkin@MacBook-Air-Gennadij.local>
2024-03-09 21:00:05 +0300

    ширина

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿public class RectangleArea
+{
+    public static void Main()
+    {
+        Console.Write("Длина: ");
+        int sideA = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ширина: ");

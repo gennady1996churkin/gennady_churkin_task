bbbecd00451f17074afc73377dcfd482c28025e9
Геннадий Чуркин <gennadijcurkin@MacBook-Air-Gennadij.local>
2024-03-09 21:05:33 +0300

    вычисления

--- Program.cs
+++ Program.cs
@@ -8,4 +8,6 @@
          int sideB = Convert.ToInt32(Console.ReadLine());
         Console.Write(" Высота: ");
         int sideC = Convert.ToInt32(Console.ReadLine());
+        int area1= sideA * sideB;
+        int area2= sideA * sideB*sideC;
 
